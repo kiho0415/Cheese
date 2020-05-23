943f7d4cdd52f3c5638023afb51432045b4ac543
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-05-23 16:43:51 +0900

    初めてーのコミット

---
+++ Cheese/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Cheese
+//
+//  Created by 森田貴帆 on 2020/05/23.
+//  Copyright © 2020 森田貴帆. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
